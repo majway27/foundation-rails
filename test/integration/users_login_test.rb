51339bf6897d1f752db59b95d3ebd5594c0c2311
rmay <majway27.1@gmail.com>
2016-08-18 11:36:18 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:rob)
+    @user = users(:admin)
   end
   
   test "login with invalid information" do
